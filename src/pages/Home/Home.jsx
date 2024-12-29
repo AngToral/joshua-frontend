258ab59387afbb9273210e1488b5bcc6cbc75f14
Angela Toral <158821520+AngToral@users.noreply.github.com>
2024-12-29 21:29:42 +0100

    start home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,21 @@
-
+import './home.css'
 
 const Home = () => {
     return (
         <>
-            <div className="flex flex-wrap text-red-600">
-                <p className="text-red-600">Sobre mí</p>
-                <p>Servicios</p>
-                <p>Contacto</p>
-                <p>Quiero mi plaza</p>
-            </div>
-            <div>
+            <div className="sticky top-0 z-50">
+                <div className="navbar flex flex-wrap justify-around h-[70px] items-center text-xl">
+                    <a>
+                        <img src="Gris.png" alt="logo" className='h-16 w-16' />
+                    </a>
+                    <a>Sobre mí</a>
+                    <a>Servicios</a>
+                    <a>Contacto</a>
+                    <a>Quiero mi plaza</a>
+                    <a>Login</a>
+                </div>
+            </div >
+            <div className="flex justify-center items-center text-lg encabezado">
                 <p>Home</p>
             </div>
         </>
