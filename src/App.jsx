55c1dbdd2bdbbda73b36afa35ea7ed267683dbb1
Angela Toral <158821520+AngToral@users.noreply.github.com>
2024-12-29 19:53:36 +0100

    before installing tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
+import Home from './pages/Home/Home'
+
 
 function App() {
 
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+
+
+const Home = () => {
+    return (
+        <>
+            <div className="flex flex-wrap text-red-600">
+                <p className="text-red-600">Sobre mí</p>
+                <p>Servicios</p>
+                <p>Contacto</p>
+                <p>Quiero mi plaza</p>
+            </div>
+            <div>
+                <p>Home</p>
+            </div>
+        </>
+    )
+}
+
+export default Home
