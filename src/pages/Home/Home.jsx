c18eafa07a15e1a3804d7a77a8a8bcf5669b59b7
Angela Toral <158821520+AngToral@users.noreply.github.com>
2025-01-01 11:06:26 +0100

    change logo, add colors, style home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,51 @@
+import { useEffect, useState } from 'react';
 import './home.css'
 
 const Home = () => {
+
+    const [scrolling, setScrolling] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    const handleScroll = () => {
+        if (window.scrollY > 0) {
+            setScrolling(true);
+        } else {
+            setScrolling(false);
+        }
+    };
+
     return (
         <>
-            <div className="sticky top-0 z-50">
-                <div className="navbar flex flex-wrap justify-around h-[70px] items-center text-xl">
-                    <a>
-                        <img src="Gris.png" alt="logo" className='h-16 w-16' />
-                    </a>
-                    <a>Sobre mí</a>
-                    <a>Servicios</a>
-                    <a>Contacto</a>
-                    <a>Quiero mi plaza</a>
-                    <a>Login</a>
+            <div className='encabezado'>
+                <div className="sticky top-0 z-50">
+                    <div className={
+                        scrolling ?
+                            "headerOnScroll flex flex-wrap justify-around h-[70px] items-center text-xl" :
+                            "header flex flex-wrap justify-around h-[70px] items-center text-xl"
+                    }>
+                        <a>
+                            <img src="guanteGris.png" alt="logoJoshua" className='h-14 w-10 cursor-pointer' />
+                        </a>
+                        <a className='link'>Sobre mí</a>
+                        <a className='link'>Servicios</a>
+                        <a className='link'>Contacto</a>
+                        <a className='link'>Quiero mi plaza</a>
+                        <a className='link'>Login</a>
+                    </div>
+                </div >
+                <div className="flex flex-col justify-center items-start h-screen ml-16 mt-32">
+                    <p className='font-bold text-6xl mb-6'>¡Tu mejor versión te espera!</p>
+                    <h1 className='font-bold text-4xl mb-6'>Soy Joshua, boxeador y entrenador personal profesional</h1>
+                    <p className='text-2xl  mb-6'>Descubre entrenamientos efectivos para alcanzar tus objetivos de fitness. ¡Comienza tu transformación hoy mismo!</p>
+                    <button className='buttonLink buttonCallToAction'>¡Empieza ya!</button>
                 </div>
-            </div >
-            <div className="flex justify-center items-center text-lg encabezado">
-                <p>Home</p>
+            </div>
+            <div className="flex justify-center items-center text-lg">
+                <p>Sección 2</p>
             </div>
         </>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        joshua: {
+          50: "#e6eae9", //gris claro
+          100: "#979ca2", //gris
+          200: "#545b66", //gris oscuro
+          300: "#2f77c3", //azul claro
+          400: "#1126a5", //azul real
+          500: "#16375f", //azul oscuro
+          600: "#031730", //azul muy oscuro
+          700: "#010a13", //azul casi negro
+          800: "#242424", //negro gris
+        },
+      }
+    },
   },
   plugins: [],
 }
