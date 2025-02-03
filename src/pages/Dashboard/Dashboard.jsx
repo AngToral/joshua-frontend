21dec7c624d47c522c109657679ec20399e2251a
Angela Toral <158821520+AngToral@users.noreply.github.com>
2025-02-03 22:24:46 +0100

    agrego filter search de videos

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,14 +1,20 @@
-import { message } from 'antd';
+import { Empty, Select, message } from 'antd';
 import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { authContext } from '../../components/context/authContext';
 import { getUserId } from '../../apiService/userApi';
 import { getTrainings } from '../../apiService/trainingApi';
 import CardsTrainings from '../../components/training/cardsTrainings';
+import { useForm } from 'react-hook-form';
+import './dashboard.css'
 
 const Dashboard = () => {
     const [userPic, setUserPic] = useState("")
     const [allTrainings, setAllTrainings] = useState([]);
+    const [filtering, setFiltering] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const { register, handleSubmit, reset, formState: { errors } } = useForm();
 
     const navigate = useNavigate();
     const [messageApi, contextHolder] = message.useMessage();
@@ -36,12 +42,32 @@ const Dashboard = () => {
     };
 
     const getAllVideos = async () => {
-        // setLoading(true)
+        setLoading(true)
         const response = await getTrainings();
         const notRemoved = response.filter((training) => !training.removeAt);
         if (response.length) setAllTrainings(notRemoved);
-        // setLoading(false);
+        setLoading(false);
     };
+
+    const onSubmitSearch = (data) => {
+        console.log(data)
+        const newList = [...allTrainings];
+        if (data.categorySearch === "All") {
+            setFiltering([]);
+        }
+        if (data) {
+            setLoading(true)
+            const filteredList = newList.filter((video) => video.category === data.categorySearch);
+            if (filteredList.length) {
+                setFiltering(filteredList);
+            } else {
+                messageApi.error("No videos found");
+                getAllVideos();
+            }
+            setLoading(false);
+        }
+    }
+
 
     return (
         <div className="h-screen">
@@ -61,14 +87,27 @@ const Dashboard = () => {
                     <a onClick={handleProfile}>Profile</a>
                 </div>
             </div>
+            <form className='flex justify-center' onChange={handleSubmit(onSubmitSearch)}>
+                <select name="Categories" className='h-10 w-40 p-2 rounded-lg bg-gray-800' {...register("categorySearch")} >
+                    <option value="All">All</option>
+                    <option value="Cardio">Cardio</option>
+                    <option value="Boxing">Boxing</option>
+                    <option value="GYM">GYM</option>
+                    <option value="Nutrition">Nutrition</option>
+                    <option value="Injuries">Injuries</option>
+                    <option value="Stretching">Stretching</option>
+                </select>
+            </form>
             <div className="flex flex-col items-center">
                 <div className='m-10 flex justify-center flex-wrap gap-4'>
-                    {allTrainings.map(video =>
-                        <CardsTrainings
-                            key={video._id}
-                            video={video}
-                        />
-                    )
+                    {allTrainings.length === 0 ? <Empty /> :
+                        (loading ? <div class="loader"></div> :
+                            (filtering.length > 0 ? filtering : allTrainings).map(video =>
+                                <CardsTrainings
+                                    key={video._id}
+                                    video={video}
+                                />
+                            ))
                     }
                 </div>
             </div>
