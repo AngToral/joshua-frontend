e30698eb430ac14f7ab2a5b2e9f875fb95236dd6
Angela Toral <158821520+AngToral@users.noreply.github.com>
2025-02-11 19:45:45 +0100

    allow fullscreen

--- src/components/training/CardsTrainings.jsx
+++ src/components/training/CardsTrainings.jsx
@@ -7,7 +7,7 @@ const CardsTrainings = ({ video }) => {
     return (
         <>
             <div className='card flex flex-col gap-1'>
-                <iframe className='rounded-xl' width="400" height="200" src={video.url} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                <iframe className='rounded-xl' width="400" height="200" src={video.url} frameborder="0" allow="fullscreen"></iframe>
                 <p className='text-xl font-bold mt-2'>{video.tittle}</p>
                 <p>Category: {video.category}</p>
                 <p>{video.description}</p>
