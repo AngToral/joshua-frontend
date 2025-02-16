1d68e4dd8c6f2422c36d6c9309350863b5997267
Angela Toral <158821520+AngToral@users.noreply.github.com>
2025-02-16 19:18:55 +0100

    clientes editables

--- src/components/clients/CardsClients.jsx
+++ src/components/clients/CardsClients.jsx
@@ -1,26 +1,44 @@
 import { FaExclamationTriangle, FaRegEdit } from "react-icons/fa"
 import { RiDeleteBinLine } from "react-icons/ri"
 import './cardsClients.css'
-import { deleteUser } from "../../apiService/userApi"
+import { deleteUser, updateUser } from "../../apiService/userApi"
 import { message } from "antd"
 import { Popover, PopoverContent, PopoverHandler, button } from "@material-tailwind/react"
 import { useForm } from "react-hook-form"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { FaRegCircleCheck } from "react-icons/fa6";
 import { IoMdArrowRoundBack } from "react-icons/io";
 
 const CardsClients = ({ client, refresh, clientId, visible }) => {
 
-    const { register, handleSubmit, setValue, reset, formState: { errors } } = useForm();
+    const { register, handleSubmit, setValue, reset, formState: { errors } } = useForm({
+        defaultValues: {
+            name: client.name,
+            lastname: client.lastname,
+            email: client.email,
+            plan: client.plan,
+        }
+    });
+
+    useEffect(() => {
+        reset({
+            name: client.name,
+            lastname: client.lastname,
+            email: client.email,
+            plan: client.plan,
+            status: client.status
+        });
+    }, [client, reset]);
+
     const [inputAble, setInputAble] = useState(true);
     const [selectedClient, setSelectedClient] = useState(null);
-
+    const [messageApi, contextHolder] = message.useMessage();
 
     const onDelete = async (id) => {
         console.log("elimino id: ", id)
         await deleteUser(id)
         refresh(prev => !prev);
-        message.success("Client deleted!")
+        message.success("Client deleted successfully!")
     }
 
     const onEdit = (id) => {
@@ -32,6 +50,8 @@ const CardsClients = ({ client, refresh, clientId, visible }) => {
     const onSubmit = async (data) => {
         console.log(data)
         setInputAble(!inputAble);
+        await updateUser(selectedClient, data)
+        message.success("Client edited successfully!")
     }
 
     return (
@@ -60,24 +80,24 @@ const CardsClients = ({ client, refresh, clientId, visible }) => {
                     <form onSubmit={handleSubmit(onSubmit)} >
                         <div className="flex flex-col">
                             <p><strong>Name: </strong>
-                                <input disabled={inputAble} value={" " + client.name} {...register("name", { required: true })} className={inputAble ? 'bg-transparent border-transparent border-[1px] font-light' : 'rounded-lg border-[1px] font-light'} />
+                                <input disabled={inputAble} {...register("name", { required: true })} className={inputAble ? 'bg-transparent border-transparent border-[1px] font-light' : 'rounded-lg border-[1px] font-light'} />
                             </p>
                             <p><strong>Lastname: </strong>
-                                <input disabled={inputAble} value={" " + client.lastname} {...register("lastname", { required: true })} className={inputAble ? 'bg-transparent border-transparent border-[1px] font-light' : 'rounded-lg border-[1px] font-light'} />
+                                <input disabled={inputAble} {...register("lastname", { required: true })} className={inputAble ? 'bg-transparent border-transparent border-[1px] font-light' : 'rounded-lg border-[1px] font-light'} />
                             </p>
                             <p><strong>Email: </strong>
-                                <input disabled={inputAble} value={" " + client.email} {...register("email", { required: true })} className={inputAble ? 'bg-transparent border-transparent border-[1px] font-light' : 'rounded-lg border-[1px] font-light'} />
+                                <input disabled={inputAble} {...register("email", { required: true })} className={inputAble ? 'bg-transparent border-transparent border-[1px] font-light' : 'rounded-lg border-[1px] font-light'} />
                             </p>
                             <p><strong>Plan: </strong>
-                                <input disabled={inputAble} value={" " + client.plan} {...register("plan", { required: true })} className={inputAble ? 'bg-transparent border-transparent border-[1px] font-light' : 'rounded-lg border-[1px] font-light'} />
+                                <input disabled={inputAble} {...register("plan", { required: true })} className={inputAble ? 'bg-transparent border-transparent border-[1px] font-light' : 'rounded-lg border-[1px] font-light'} />
                             </p>
                             <p><strong>Status: </strong>
                                 {client.status === "active" ? "🟢" : "🔴"}
                                 <input disabled value={" " + client.status} {...register("status", { required: true })} className='bg-transparent border-transparent border-[1px] font-light' />
                             </p>
-                            {!inputAble ?
+                            {!inputAble &&
                                 <button className="flex justify-end" type='submit'><FaRegCircleCheck className='h-5 w-5' /></button>
-                                : null}
+                            }
                         </div>
                     </form>
                 </div>
